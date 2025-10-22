a57e44650f5b2c0c87cefe68f0e3a1877666ba73
ryodes <k2acim31@gmail.com>
2025-10-22 23:25:55 +0200

    add origin cors

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ def create_app():
     app = Flask(__name__)
     CORS(
         app,
-        resources={r"/*": {"origins": ["http://localhost:3000", "http://127.0.0.1:3000"]}},
+        resources={r"/*": {"origins": ["http://localhost:3000", "http://127.0.0.1:3000", "https://cookmemo.vercel.app"]}},
         supports_credentials=True,
         allow_headers=["Content-Type", "Authorization"],
         methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"]
