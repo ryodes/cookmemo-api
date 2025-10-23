23155047903306a6c63e5143bb66410a751c60f5
ryodes <k2acim31@gmail.com>
2025-10-23 08:33:43 +0200

    new wsgi & new requiement

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,11 @@
-from app import create_app
+from app import create_app, db
+from app.models import User  # ou n'importe quelle table
 
 app = create_app()
+
+with app.app_context():
+    if not db.inspect(db.engine).has_table("user"):
+        db.create_all()
 
 if __name__ == "__main__":
     app.run()
